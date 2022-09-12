b50382d0418f057187f1ae908901dae221fa31cf
Nitsan Baleli <nitsan.baleli@lemonade.com>
2022-09-12 17:10:41 +0300

    fix links, images

--- data/siteConfig.ts
+++ data/siteConfig.ts
@@ -1,20 +1,19 @@
 import type { SiteConfig } from "@/lib/types";
 const siteConfig: SiteConfig = {
-  avatar: "/avatar.png",
-  siteUrl: "https://nextjs-mdx-blog-theme.vercel.app",
-  siteName: "Alex Carpenter",
+  avatar: "./avatar.png",
+  siteUrl: "https://nitsanbaleli.github.io/nextjs-blog",
+  siteName: "Nitsan Baleli",
   siteDescription:
     "Starter template for a personal website blog, built with Next.js, MDX, and Tailwind CSS.",
-  siteThumbnail: "/og-image.png",
+  siteThumbnail: "./og-image.png",
   nav: [
     { label: "Posts", href: "/posts" },
     { label: "About", href: "/about" },
   ],
   social: {
-    github: "https://github.com/alexcarpenter",
-    twitter: "https://twitter.com/hyrbid_alex",
-    linkedin: "https://www.linkedin.com/in/imalexcarpenter/",
-    instagram: "https://www.instagram.com/alexcarp/",
+    github: "https://github.com/NitsanBaleli",
+    twitter: "https://twitter.com/nbaleli",
+    linkedin: "https://www.linkedin.com/in/nbaleli",
   },
 };
 export default siteConfig;
